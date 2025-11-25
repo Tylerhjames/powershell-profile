9f39c004b7b306554a8c626744b903ef1622e5ee
Tyler <tyler@gocbit.com>
2025-11-25 08:47:30 -0800

    added shortcut to git repo profile

---
+++ Functions/gitprofile.ps1
@@ -0,0 +1,3 @@
+# Open the Git-for-Windows PowerShell profile in Notepad++
+function gitprofile { npp "C:\Users\tyler\Documents\Git\powershell-profile\Profile.ps1" }
+Set-Alias gprof gitprofile   # optional super-short alias
