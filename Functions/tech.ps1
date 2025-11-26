ac4f989b81ade2b150d7f875c839889253b94373
Tyler <tyler@gocbit.com>
2025-11-25 17:28:10 -0800

    removed auto update feature

---
+++ Functions/tech.ps1
@@ -0,0 +1,17 @@
+function tech {
+    Write-Host "`n=== CBIT Technician Menu ===" -ForegroundColor Cyan
+    Write-Host "1) Net-Test"
+    Write-Host "2) Public IP"
+    Write-Host "3) Renew Network"
+    Write-Host "4) Flush DNS"
+    Write-Host "5) Check Printer Subnets"
+    Write-Host "Q) Quit"
+
+    switch (Read-Host "Select") {
+        "1" { ntest -Launcher }
+        "2" { publicip }
+        "3" { renew-safe }
+        "4" { ipconfig /flushdns }
+        "5" { Find-Printers }
+    }
+}
