462ed0e3f7b6242ed970f4b6d952e181f9400edb
Tyler <tyler@gocbit.com>
2025-11-26 20:40:43 -0800

    Update Tech menu options

--- Functions/tech.ps1
+++ Functions/tech.ps1
@@ -4,14 +4,14 @@ function tech {
     Write-Host "2) Public IP"
     Write-Host "3) Renew Network"
     Write-Host "4) Flush DNS"
-    Write-Host "5) Check Printer Subnets"
+    Write-Host "5) Check Email DNS"
     Write-Host "Q) Quit"
 
     switch (Read-Host "Select") {
-        "1" { ntest -Launcher }
+        "1" { Net-Test }
         "2" { publicip }
         "3" { renew-safe }
         "4" { ipconfig /flushdns }
-        "5" { Find-Printers }
+        "5" { Test-EmailDNS }
     }
 }
